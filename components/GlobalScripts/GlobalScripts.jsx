e0a82cc1fa03e19f27b936c29ee71ba6dd4df626
ahsankhan201010 <ahsankhan201010@gmail.com>
2022-02-05 18:43:29 +0500

    optimized scripts except gtag

---
+++ components/GlobalScripts/GlobalScripts.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import Script from "next/script";
+
+const GlobalScripts = () => {
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          document.addEventListener("DOMContentLoaded", function(event) {
+            setTimeout(function() {
+              try {
+              var script = document.createElement('script');
+              //script.defer = true; 
+              script.type = 'text/javascript';
+              script.onload = function() {
+                console.log("hello clickease!!")
+              }
+              var target = 'https://www.clickcease.com/monitor/stat.js';
+              script.src = target;
+              var elem = document.head;
+              elem.appendChild(script);
+              } catch (error) {
+              console.log(error)
+              }
+            }, 1000)
+          });
+          `,
+        }}
+      />
+
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          document.addEventListener("DOMContentLoaded", function(event) {
+            setTimeout(function() {
+              try {
+              var script = document.createElement('script');
+              //script.defer = true; 
+              script.type = 'text/javascript';
+              script.onload = function() {
+                console.log("hello clickease!!")
+              }
+              var target = 'https://www.clickcease.com/monitor/stat.js';
+              script.src = target;
+              var elem = document.head;
+              elem.appendChild(script);
+              } catch (error) {
+              console.log(error)
+              }
+            }, 1000)
+          });
+          `,
+        }}
+      />
+      <Script
+        strategy="afterInteractive"
+        src="https://widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js"
+      />
+      <Script
+        strategy="afterInteractive"
+        src="https://tools.luckyorange.com/core/lo.js?site-id=c08d05c9"
+      />
+    </>
+  );
+};
+
+export default GlobalScripts;
--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,12 @@
 import dynamic from "next/dynamic";
 import Navbar from "../components/Navbar/Navbar";
 import "../styles/globals.scss";
-import Script from "next/script";
 import { useRouter } from "next/router";
 import ContactHeader from "../components/ContactHeader/ContactHeader";
 import ContactWithUs from "../components/ContactWithUs/ContactWithUs";
-
 import ContactHeaderForLanding from "../components/ContactHeaderForLanding/ContactHeaderForLanding";
-
-import { openLiveChat } from "../utils/Data/helpers";
 import { useEffect } from "react";
-import Head from "next/head";
+import GlobalScripts from "../components/GlobalScripts/GlobalScripts";
 const Footer = dynamic(() => import("../components/Footer/Footer"));
 const Copyright = dynamic(() => import("../components/Copyright/Copyright"));
 const CopyrightForLanding = dynamic(() =>
@@ -55,14 +51,6 @@ function MyApp({ Component, pageProps }) {
   }, []);
   return (
     <div style={{ position: "relative" }}>
-      <Head>
-        {/* <script src="https://widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" defer/>
-      <script src="https://tools.luckyorange.com/core/lo.js?site-id=c08d05c9" defer/>
-      <script src="https://www.clickcease.com/monitor/stat.js" defer/> */}
-        {/* <Script strategy="afterInteractive" src="https://widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" />
-      <Script strategy="afterInteractive" src="https://tools.luckyorange.com/core/lo.js?site-id=c08d05c9"/>
-      <Script strategy="afterInteractive" src="https://www.clickcease.com/monitor/stat.js"/> */}
-      </Head>
       <ContactWithUs />
       {isLandingPage(pathname) ? (
         <ContactHeaderForLanding />
@@ -77,52 +65,14 @@ function MyApp({ Component, pageProps }) {
       >
         <Component {...pageProps} />
       </div>
+      <GlobalScripts/>
       {/* <Script
         strategy="afterInteractive"
         id="ze-snippet"
         src="https://static.zdassets.com/ekr/snippet.js?key=026cd8b7-0c2c-42c5-a8ef-6bd02caf68fb"
         // onLoad={openLiveChat}
       /> */}
-      {/* <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-391VCNVG9G"
-        onLoad={() => {
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-391VCNVG9G');
-        }}
-      /> */}
-      <Script
-        strategy="afterInteractive"
-        src="https://widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js"
-      />
-      <Script
-        strategy="afterInteractive"
-        src="https://tools.luckyorange.com/core/lo.js?site-id=c08d05c9"
-      />
-      <Script
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-          document.addEventListener("DOMContentLoaded", function(event) {
-          setTimeout(function() {
-          var script = document.createElement('script'); 
-          script.defer = true;
-          script.type = 'text/javascript';
-          var target = 'https://www.clickcease.com/monitor/stat.js'; 
-          script.src = target;
-          var elem = document.head;
-          elem.appendChild(script);
-      }, 0)
-  `,
-        }}
-      />
-      {/* <Script
-        strategy="afterInteractive"
-        src="https://www.clickcease.com/monitor/stat.js"
-      /> */}
+    
       {isLandingPage(pathname) ? null : <Footer />}
       {isLandingPage(pathname) ? <CopyrightForLanding /> : <Copyright />}
     </div>
