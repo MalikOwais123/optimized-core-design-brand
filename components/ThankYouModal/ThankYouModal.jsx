2a27e3dd37a05ee3d4100e1ecdc8659263de2f72
hmrizwan98 <sendtorizwan19@gmail.com>
2022-02-10 16:13:20 +0500

    thank you modal

--- components/ThankYouModal/ThankYouModal.jsx
+++ components/ThankYouModal/ThankYouModal.jsx
@@ -1,13 +1,27 @@
 import Modal from "../Modal/Modal";
 import classList from "./ThankYouModal.module.scss";
 
-
 const ThankYouModal = () => {
   return (
     <Modal>
-        dskl;ask
+      <div className={classList.wizard}>
+        <div className={classList.gridWizard}>
+          <div className={classList.cheel}>
+            <img src={optimize.src} alt="" />
+          </div>
+          <div className={classList.contentWrepper}>
+            <Header
+              style={{ marginBottom: 30 }}
+              fontWeight="semi-bold"
+              fontSize={20}>
+              {title}
+            </Header>
+          </div>
+        </div>
+        <div className={classList.submit_action}></div>
+      </div>
     </Modal>
-  )
-}
+  );
+};
 
-export default ThankYouModal
+export default ThankYouModal;
