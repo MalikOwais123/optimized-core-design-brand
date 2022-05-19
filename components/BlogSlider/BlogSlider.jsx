bf2b1aa9ea6d72a8ce9a7e1cdf8d7917717043a3
hmrizwan98 <sendtorizwan19@gmail.com>
2022-05-19 16:03:49 +0500

    resolve error

--- components/BlogSlider/BlogSlider.jsx
+++ components/BlogSlider/BlogSlider.jsx
@@ -24,7 +24,6 @@ const BlogSlider = ({ blogsData, handleBlogClick }) => {
     }
     return "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
   };
-  console.log("blogsData", blogsData);
 
   return (
     <>
